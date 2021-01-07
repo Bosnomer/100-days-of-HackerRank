d31bb29f35ed0ba544db1dddb1c688813d621bf7
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-01-07 11:32:25 +0530

    Update day3.py

--- Python/day3.py
+++ Python/day3.py
@@ -1,8 +1,8 @@
 # Arithmetic Operators
 
 if __name__ == '__main__':
-    a = int(raw_input())
-    b = int(raw_input())
+    a = int(input())
+    b = int(input())
     if ((a,b>=1) and (a,b<=10**10)):
         print(a+b)
         print(a-b)
