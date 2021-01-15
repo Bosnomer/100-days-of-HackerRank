e19b8f2cea15f16f713675c809e4b71f9e061f57
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-01-15 22:47:21 +0530

    Create day15.py

---
+++ Python/day15.py
@@ -0,0 +1,9 @@
+# What's Your Name?
+
+def print_full_name(a, b):
+    print("Hello "+str(a), str(b)+"! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
