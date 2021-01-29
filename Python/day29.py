9bf4c1137eecfdbf01c9473c0b17b83316f7945a
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-01-29 23:40:09 +0530

    Create day29.py

---
+++ Python/day29.py
@@ -0,0 +1,10 @@
+# Incorrect Regex
+
+import re
+for _ in range(int(input())):
+    ans = True
+    try:
+        reg = re.compile(input())
+    except re.error:
+        ans = False
+    print(ans)
