6f998a02afef5e7b941eef2e64951108d5fad89f
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-02-16 19:37:23 +0530

    Create day47.sql

---
+++ SQL/day47.sql
@@ -0,0 +1,5 @@
+# Weather Observation Station 20
+
+SELECT ROUND(s.lat_n,4) FROM station s 
+WHERE (SELECT ROUND(COUNT(s.id)/2)-1 FROM station) = 
+    (SELECT COUNT(s1.id) FROM station s1 WHERE s1.lat_n > s.lat_n);
