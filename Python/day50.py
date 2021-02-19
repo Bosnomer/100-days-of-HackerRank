d5e2b885c64e137bebeaae7512e696bdea2a558e
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-02-19 20:44:36 +0530

    Create day50.py

---
+++ Python/day50.py
@@ -0,0 +1,10 @@
+# Set Mutations
+
+if __name__ == '__main__':
+    (_, A) = (int(input()),set(map(int, input().split())))
+    B = int(input())
+    for _ in range(B):
+        (command, newSet) = (input().split()[0],set(map(int, input().split())))
+        getattr(A, command)(newSet)
+
+    print (sum(A))
