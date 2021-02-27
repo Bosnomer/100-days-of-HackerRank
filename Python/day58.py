c05c22d59dfbfc51ef72d6f00619e721d673c474
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-02-27 14:32:20 +0530

    Create day58.py

---
+++ Python/day58.py
@@ -0,0 +1,10 @@
+# Zipped!
+
+n, x = map(int, input().split()) 
+
+sheet = []
+for _ in range(x):
+    sheet.append( map(float, input().split()) ) 
+
+for i in zip(*sheet): 
+    print( sum(i)/len(i) )
