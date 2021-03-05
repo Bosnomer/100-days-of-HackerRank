bc72d707992ee5b7833cbec51f3bd87ae05af161
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-03-05 22:22:02 +0530

    Create day64.py

---
+++ Python/day64.py
@@ -0,0 +1,8 @@
+# Transpose and Flatten
+
+import numpy
+
+n, m = map(int, input().split())
+array = numpy.array([input().strip().split() for _ in range(n)], int)
+print (array.transpose())
+print (array.flatten())
