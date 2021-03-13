0909dfb7487fde236fc7e9e0ca944c640ed331e5
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-03-13 22:34:05 +0530

    Create day72.py

---
+++ Python/day72.py
@@ -0,0 +1,8 @@
+# Dot and Cross
+
+import numpy
+
+n = int(input())
+a = numpy.array([input().split() for _ in range(n)], int)
+b = numpy.array([input().split() for _ in range(n)], int)
+print (numpy.dot(a, b))
