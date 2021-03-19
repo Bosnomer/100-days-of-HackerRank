c1a694c68e33e3702b6b23fe442cd6049332cdf3
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-03-19 23:21:21 +0530

    Create day78.py

---
+++ Python/day78.py
@@ -0,0 +1,7 @@
+# Re.findall() & Re.finditer()
+
+import re
+v = "aeiou"
+c = "qwrtypsdfghjklzxcvbnm"
+m = re.findall(r"(?<=[%s])([%s]{2,})[%s]" % (c, v, c), input(), flags = re.I)
+print('\n'.join(m or ['-1']))
