ce212862a0f0dcb1f0f803d43d91e452248efc04
Nikhil Kesarkar <nikhilkesarkar47@gmail.com>
2021-03-20 15:43:45 +0530

    Create day79.py

---
+++ Python/day79.py
@@ -0,0 +1,13 @@
+# Re.start() & Re.end()
+
+import re
+
+S = input()
+k = input()
+
+pattern = re.compile(k)
+r = pattern.search(S)
+if not r: print ("(-1, -1)")
+while r:
+    print ("({0}, {1})".format(r.start(), r.end() - 1))
+    r = pattern.search(S,r.start() + 1)
